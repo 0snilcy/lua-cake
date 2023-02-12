9037e0c70d66364b9c87829e36429e40ba40ca50
Johnny Rocket <0.snilcy@gmail.com>
2023-02-12 16:27:24 +0300

    update

---
+++ lib/dev.lua
@@ -0,0 +1,9 @@
+is = require("lib.type")
+tbl = require("lib.table")
+log = require("lib.print")
+str = require("lib.string")
+cls = require("lib.class")
+
+expect = require("lib.test").expect
+test = require("lib.test").test
+describe = require("lib.test").describe
--- lib/prod.lua
+++ lib/prod.lua
@@ -1,4 +1,8 @@
+is = require("lib.type")
+
+tbl = require("lib.table")
 is = require("cake.type")
+
 tbl = require("cake.table")
 log = require("cake.print")
 str = require("cake.string")
@@ -7,3 +11,10 @@ cls = require("cake.class")
 expect = require("cake.test").expect
 test = require("cake.test").test
 describe = require("cake.test").describe
+log = require("lib.print")
+str = require("lib.string")
+cls = require("lib.class")
+
+expect = require("lib.test").expect
+test = require("lib.test").test
+describe = require("lib.test").describe
